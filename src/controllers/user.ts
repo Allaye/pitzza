df773beb476aa70b81e4e9c10e201ff78fc1cac0
Allaye <kolade199@gmail.com>
2021-12-12 14:05:04 +0100

    implement user controller functions

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,12 @@
-import mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import User  from '../models/user';
 
 
 const register = async (req: Request, res: Response) => {
-    const user = new User(req.body);
     try {
-        const user = await U
-        await user.save();
+        const user = new User(req.body);
         const token = await user.generateAuthToken();
+        await user.save();
         res.status(201).send({ user, token });
     } catch (error) {
         res.status(400).send(error);

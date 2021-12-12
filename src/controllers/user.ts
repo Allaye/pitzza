67d2a76d980f1f77b2d2f9e1d88104208744a053
Allaye <kolade199@gmail.com>
2021-12-12 23:11:25 +0100

    update users controller functions

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,13 +2,56 @@ import { Request, Response } from 'express';
 import User  from '../models/user';
 
 
-const register = async (req: Request, res: Response) => {
+
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        [x: string]: any;
+        user: any
+    }
+    // interface Response {
+    //     error: (code: number, message: string) => Response;
+    //     success: (code: number, message: string, result: any) => Response
+    // }
+}
+
+
+
+export const register = async (req: Request, res: Response) => {
     try {
         const user = new User(req.body);
-        const token = await user.generateAuthToken();
+        // console.log(user);
+        const token = await user.schema.methods.generateAuthToken(user);
+        // console.log(token);
         await user.save();
         res.status(201).send({ user, token });
     } catch (error) {
         res.status(400).send(error);
     }
 }
+
+export const login = async (req: Request, res: Response) => {
+    try{
+        console.log(req.body);
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        console.log(user);
+        const token = await user.schema.methods.generateAuthToken(user);
+        res.status(200).send({ user, token });
+    }catch(err){
+        res.status(400).send(err);
+    } 
+}
+
+export const logout = async (req: Request, res: Response) => {
+    try{
+        console.log(req.user);
+        req.user.tokens = req.user.tokens.filter((token: { token: any; }) => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+        res.send();
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+}   

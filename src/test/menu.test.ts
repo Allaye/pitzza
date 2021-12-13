1c39679ee859b5bf8795630747d13586cf162f4b
Allaye <kolade199@gmail.com>
2021-12-13 15:48:29 +0100

    testing

--- src/test/menu.test.ts
+++ src/test/menu.test.ts
@@ -2,8 +2,28 @@ import request from 'supertest';
 import app from '../app';
 
 
-describe("test creating menu route", () => {
-    test("return response should be 201", async()=>{
+describe("test for Menu routes", () => {
+    test("return response should be 201 and other properties", async()=>{
+        const res = await request(app).post('/create/menu').send({
+            foodname: "fufu",
+            description: "fresh fufu",
+            price: 30,
+        });
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toHaveProperty('foodname');
+        expect(res.body).toHaveProperty('price');
+        expect(res.body).toHaveProperty('description');
+    });
+
+    test("return response should be 200 and other properties", async()=>{
+        const res = await request(app).get('/get/menus');
+        expect(res.statusCode).toBe(200);
+        expect(res.body[0]).toHaveProperty('foodname');
+        expect(res.body[0]).toHaveProperty('price');
+        expect(res.body[0]).toHaveProperty('description');
+    });
+
+    test("return response should be 201 and other properties", async()=>{
         const res = await request(app).post('/create/menu').send({
             foodname: "fufu",
             description: "fresh fufu",

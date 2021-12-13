3a52f422d13f8e914f72e75f963f09767b1fa7ce
Allaye <kolade199@gmail.com>
2021-12-13 14:36:57 +0100

    convert user controller to class methods

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,13 @@
 import {Router} from "express";
 // import {addOrder, getOrders, getOrderById, updateOrder, deleteOrder} from "../controllers/order";
-import { register, login, logout, updateUser } from "../controllers/user";
+import UserController from "../controllers/user";
 import {auth} from "../middleware/auth";
 const userRouter: Router = Router();
 
-userRouter.post("/registration",  register);
-userRouter.post("/login", login);
-userRouter.post("/logout", auth, logout);
-userRouter.patch("/update/user", auth, updateUser);
+userRouter.post("/registration",  UserController.register);
+userRouter.post("/login", UserController.login);
+userRouter.post("/logout", auth, UserController.logout);
+userRouter.patch("/update/user", auth, UserController.updateUser);
 // orderRouter.delete("/delete/order/:id", deleteOrder);
 
 export = userRouter;
